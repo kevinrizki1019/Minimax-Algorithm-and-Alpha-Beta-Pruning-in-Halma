4e282bf362ed8c94f48f0c28dd6e68db43d38fec
kevinrizki1019 <13518100@std.stei.itb.ac.id>
2020-10-19 23:33:48 +0700

    Refactoring all code, move to src foder

---
+++ src/GameManager.js
@@ -0,0 +1,114 @@
+class GameManager {
+    constructor(canvas, boardSize) {
+        this.canvas = canvas;
+
+        // Board variables
+        this.boardSize = boardSize
+        this.blockSize = canvas.element.width/boardSize
+        this.boardMatrix = []
+
+        // Player variables
+        this.players = null
+        this.currentPlayer = null
+        this.nextPlayer = null
+    }
+
+    setup(player1, player2) {
+
+        // Create board
+        this.board = new Board(this)
+        this.board.draw()
+
+        for (let i = 0; i < this.boardSize; i++) {
+            let row = []
+            for (let j = 0; j < this.boardSize; j++) {
+                row.push(null)
+            }
+            this.boardMatrix.push(row)
+        }
+
+        const size = this.boardSize
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                // Posisi bidak Hijau yang ada di pojok kiri atas
+                if ((i < size / 2) && (j < size / 2) && (i + j < size / 2)) {
+                    this.createPawnAt(new Position(i, j), 'red')
+                    player1.listOfPawns.push(this.getPawnAt(new Position(i, j)))
+                } 
+                // Posisi bidak Merah ada di pojok kanan bawah (perhitungannya masih bisa salah)
+                else if ((i >= size / 2) && (j >= size / 2) && (i + j >= (size - 1) + (size / 2))) {
+                    this.createPawnAt(new Position(i, j), 'green')
+                    player2.listOfPawns.push(this.getPawnAt(new Position(i, j)))
+                } 
+            }
+        }
+
+        // Setup game
+        this.players = [player1, player2]
+        this.currentPlayer = this.players[0]
+        this.nextPlayer = this.players[1]
+
+        // setup interaction for human player
+        window.addEventListener('click', (e) => {   
+            if (this.currentPlayer instanceof Human) {
+                const x = Math.floor(e.clientX/this.blockSize)
+                const y = Math.floor(e.clientY/this.blockSize)
+            
+                let selectedPawn = this.getPawnAt(new Position(x, y)) 
+                if (selectedPawn) {
+                    if (selectedPawn.color === this.currentPlayer.color)
+                    {
+                        this.currentPlayer.selectedPawn = selectedPawn
+                        this.currentPlayer.selectedPawnValidMoves = getValidMovesPawnAt(this.boardMatrix, x, y)
+                    }
+                } else {
+                    if (this.currentPlayer.selectedPawn) {
+                        const move = new Position(x, y)
+                        let isValidMove = false
+                        for (const validMove of this.currentPlayer.selectedPawnValidMoves) {
+                            if (move.isSame(validMove)) {
+                                this.movePawnTo(this.currentPlayer.selectedPawn, move)
+                                this.currentPlayer.resetAction()
+                                this.changePlayer()
+
+                                isValidMove = true
+                                break
+                            }
+                        }
+
+
+                    }
+                }
+            }
+        })
+    }
+
+    changePlayer() {
+        const temp = this.currentPlayer
+        this.currentPlayer = this.nextPlayer
+        this.nextPlayer = temp
+        this.currentPlayer.move()
+    }
+
+    getPawnAt(post) {
+        return this.boardMatrix[post.y][post.x]
+    }
+
+    createPawnAt(post, color) {
+        let newPawn = new Pawn(this, post, color)
+        this.boardMatrix[post.y][post.x] = newPawn
+        newPawn.draw()
+    }
+
+    movePawnTo(pawn, post) {
+        this.boardMatrix[post.y][post.x] = pawn
+        this.boardMatrix[pawn.post.y][pawn.post.x] = null
+        pawn.post = post
+
+        this.board.update()
+    }
+
+    start() {
+        this.currentPlayer.move()
+    }
+}
---
+++ src/Player.js
@@ -0,0 +1,166 @@
+class Player {
+    constructor(game, color) {
+        this.game = game
+        this.color = color
+        this.listOfPawns = []
+    }
+}
+
+class Human extends Player {
+
+    constructor(game, color) {
+        super(game, color)
+        this.resetAction()
+    }
+
+    resetAction() {
+        this.selectedPawn = null
+        this.selectedTarget = null
+        this.selectedPawnValidMoves = null
+    }
+
+    move() {
+        // do nothing
+    }
+}
+
+class BotMinimax extends Player {
+    constructor(game, color) {
+        super(game, color)
+    }
+    
+    move() {
+        const game = this.game
+        console.log("minimax", this.minimax(game.boardMatrix, 0, game, true, -Infinity, Infinity))
+        let validMoves = []
+        let pawns = []
+        let currentPawn = 0
+        for (let i = 0; i < game.boardSize; i++) {
+            for (let j = 0; j < game.boardSize; j++) {
+                const pawn = game.getPawnAt(new Position(i, j))
+                if (pawn) {
+                    if (pawn.color === game.currentPlayer.color) {
+                        pawns.push(pawn)
+                    }
+                }
+            }
+        }
+        for (let i = 0; i < game.boardSize; i++) {
+            for (let j = 0; j < game.boardSize; j++) {
+                const pawn = game.getPawnAt(new Position(i, j))
+                if (pawn == null) {
+                    validMoves.push([i, j])
+                }
+            }
+        }
+        const randomPawnNum = Math.floor(Math.random() * pawns.length)
+        const randomMoveNum = Math.floor(Math.random() * validMoves.length)
+        const selectedPawn = pawns[randomPawnNum]
+        const selectedMove = validMoves[randomMoveNum]
+        game.movePawnTo(selectedPawn, new Position(selectedMove[0], selectedMove[1]))
+        game.changePlayer()
+    }
+
+    stepNeeded(from, to){
+        let disX = to.x-from.x
+        let disY = to.y-from.y
+        // return Math.abs((disX+disY)-Math.max(disX,disY))
+        let dist = Math.sqrt(Math.pow(disX, 2) + Math.pow(disY, 2))
+        return dist
+    }
+    
+    getScore(state){
+        const game = this.game
+        let poin = 0
+
+        if (this.color === game.players[0].color) {
+            for (const pawn of this.listOfPawns) {
+                poin -= this.stepNeeded(pawn.post, new Position(state.length-1, state.length-1))
+            }
+            for (const pawn of game.players[1].listOfPawns) {
+                poin += this.stepNeeded(pawn.post, new Position(0, 0))
+            }
+            return poin
+        } else {
+            for (const pawn of this.listOfPawns) {
+                poin -= this.stepNeeded(pawn.post, new Position(0, 0))
+            }
+            for (const pawn of game.players[0].listOfPawns) {
+                poin += this.stepNeeded(pawn.post, new Position(state.length-1, state.length-1))
+            }
+            return poin
+        }
+    }
+
+    minimax(state, depth, game, isMaximizing, alpha, beta) {
+        if (depth > 3) {
+            // console.log(this.getScore(state))
+            return this.getScore(state)
+        }
+        
+        if (isMaximizing) {
+            let bestScore = -Infinity
+            let bestMove = null
+            
+            for (const pawn of game.currentPlayer.listOfPawns) {
+                const [x, y] = [pawn.post.x, pawn.post.y]
+                const moves = getValidMovesPawnAt(state, x, y)
+                for (const move of moves) {
+                    if (isValidPosition(state, move)) {
+                        if (isEmptyCell(state, move)) {
+                            const copiedState = copyState(state)
+                            copiedState[move.y][move.x] = new Pawn(new Position(move.x, move.y), pawn.color)
+                            copiedState[y][x] = null
+                            const score = this.minimax(copiedState, depth+1, game, false, alpha, beta)
+                            if (score > bestScore) {
+                                bestScore = score
+                                bestMove = {pawn: pawn, move: move}
+                            }
+                            alpha = Math.max(alpha, bestScore)
+                            
+                            if (beta <= alpha) 
+                                break
+                        }
+                    }
+                }
+            }
+
+            if (depth === 0) {
+                console.log(bestScore)
+                return bestMove
+            } 
+                
+            return bestScore
+        } else {
+            let bestScore = Infinity
+            for (const pawn of game.currentPlayer.listOfPawns) {
+                const [x, y] = [pawn.post.x, pawn.post.y]
+                const moves = getValidMovesPawnAt(state, x, y)
+                for (const move of moves) {
+                    if (isValidPosition(state, move)) {
+                        if (isEmptyCell(state, move)) {
+                            const copiedState = copyState(state)
+                            copiedState[move.y][move.x] = new Pawn(new Position(move.x, move.y), pawn.color)
+                            copiedState[y][x] = null
+                            const score = this.minimax(copiedState, depth+1, game, true, alpha, beta)
+                            bestScore = Math.min(bestScore, score)
+                            beta = Math.min(beta, bestScore)
+                            
+                            if (beta <= alpha) 
+                                break
+                        }
+                    }
+                }
+            }
+            return bestScore
+        }
+    }
+
+    move() {
+        const game = this.game
+        const optimalMove = this.minimax(game.boardMatrix, 0, game, true, -Infinity, Infinity)
+        console.log(optimalMove)
+        game.movePawnTo(optimalMove.pawn, optimalMove.move)
+        game.changePlayer()
+    }
+}
